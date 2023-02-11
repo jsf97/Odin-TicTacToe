2cae8103b297083e1da13cf2cfce58147ba423e8
Juan <116412382+jsf97@users.noreply.github.com>
2023-02-11 13:25:18 -0300

    encontradn osoluciones

--- js/code.js
+++ js/code.js
@@ -14,11 +14,18 @@ const gameBoard = (() => {
   // Putting marks in cells
   let turnX = true;
   let turn0 = false;
-
+  const allX = (str) => {
+    if (str.textContent === 'x') {
+      console.log('b')
+    }
+    }
   cell.forEach((e)=> {
     e.addEventListener('click', ()=> {
       if (turnX === true && e.textContent === '') {
-        e.textContent = 'X'
+        e.textContent = 'x'
+        winCombination.forEach((e) => {
+          e.every(allX)
+        })
         turnX = false;
       }
        else if (turnX === false && e.textContent === '') {
@@ -29,19 +36,11 @@ const gameBoard = (() => {
   })
 
   // Function to check winner
-  cell[0].textContent = 'x'
-  cell[1].textContent = 'x'
-  cell[2].textContent = 'x'
   let winCombination = [
     [cell[0], cell[1], cell[2]],
-    [cell[3], cell[4], cell[5]],
-    [cell[6], cell[7], cell[8]]
+     [cell[3], cell[4], cell[5]],
+     [cell[6], cell[7], cell[8]]
   ]
-  
-  winCombination.forEach((e) => {
-    e.forEach((f) => {
-      console.log(f.textContent)
-    })
-  })
 
 })();
+
