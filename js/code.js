972693f8d7a7ec5ceb5dd9b670f3ad6ed8e229c0
Juan <116412382+jsf97@users.noreply.github.com>
2023-02-17 03:09:29 -0300

    Aplicada la lógica del tateti y el estilo del DataUI, faltan detalles

--- js/code.js
+++ js/code.js
@@ -1,4 +1,6 @@
 const screen = document.querySelector('.container')
+
+
 // GAMEBOARD UI 
 const mid = document.querySelector('.mid');
 const arrayGameboard = []
@@ -11,6 +13,8 @@ for(i = 0; i <= 8; i++) {
 }
 
 
+
+
 // FACTORY FUNCTION (must be atacched on buttons)
 function newPlayer(name, mark) {
   return {name, mark}
@@ -18,32 +22,61 @@ function newPlayer(name, mark) {
 
 // BEFORE PLAY BUTTON
 
+
+// Get players name function
+
+
 // Get mark function
-const getMark = document.querySelectorAll('.p-mark');
-getMark.forEach((e) => {
-  e.addEventListener('click', ()=> {
-    if (e === getMark[0] || e === getMark[3]) {
-      getMark[0].classList.toggle('active')
-      getMark[3].classList.toggle('active')
-      getMark[1].classList.remove('active')
-      getMark[2].classList.remove('active')
-    }
-    else if  (e === getMark[1] || e === getMark[2]) {
-      getMark[1].classList.toggle('active')
-      getMark[2].classList.toggle('active')
-      getMark[0].classList.remove('active')
-      getMark[3].classList.remove('active')
-    }
-  })
-})
+
+
+// Start playing function
+
 
 
 
 // AFTER PLAY BUTTON
+let pX = true;
+let p0 = false;
+let winCombinations = [[arrayGameboard[0], arrayGameboard[1], arrayGameboard[2]],
+                       [arrayGameboard[3], arrayGameboard[4], arrayGameboard[5]],
+                       [arrayGameboard[6], arrayGameboard[7], arrayGameboard[8]],
+                       [arrayGameboard[0], arrayGameboard[4], arrayGameboard[8]],
+                       [arrayGameboard[2], arrayGameboard[4], arrayGameboard[6]]];
+
+function checkWinX(win) {
+  return win.textContent === 'X'
+};
+
+function checkWin0(win) {
+  return win.textContent === '0'
+};
+
 (function abc() {
   arrayGameboard.forEach((e) => {
     e.addEventListener('click', ()=> {
-      e.textContent = 'a'
+      if (pX) {
+        e.textContent = 'X'
+        pX = false;
+        p0 = true;
+        for(i = 0; i < winCombinations.length; i++) {
+          if(winCombinations[i].every(checkWinX)) {
+            console.log('a')
+            p0 = false;
+          }
+        }
+      }
+      else if (p0) {
+        e.textContent = '0'
+        pX = true;
+        p0 = false;
+        for(i = 0; i < winCombinations.length; i++) {
+          if(winCombinations[i].every(checkWin0)) {
+            console.log('a')
+            pX = false;
+          }
+        }
+      }
+
     })
   })
 })()
